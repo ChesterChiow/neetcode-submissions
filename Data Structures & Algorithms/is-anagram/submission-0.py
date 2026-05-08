ab126646c8fe4bd15bcc502c476c461191d8e759
Chester Chiow <144107213+ChesterChiow@users.noreply.github.com>
2026-05-08 22:12:06 +0800

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,7 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        S = sorted(s)
-        T = sorted(t)
-        if (S == T):
-            return True
-        return False
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
