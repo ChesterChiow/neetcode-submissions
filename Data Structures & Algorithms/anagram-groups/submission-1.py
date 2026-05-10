799a81f0c8a83819005611c7cb1cfe58ef6a22e4
Chester Chiow <144107213+ChesterChiow@users.noreply.github.com>
2026-05-10 13:44:17 +0800

    Remove: anagram-groups - submission-1

--- Data Structures & Algorithms/anagram-groups/submission-1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Sort
-        result = defaultdict(list)
-        for s in strs:
-            sortedS = ''.join(sorted(s))
-            result[sortedS].append(s)
-        return list(result.values()) # convert dict to list
-       
