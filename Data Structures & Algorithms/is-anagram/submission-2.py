14b7d44d3b70e6ef44f08e799847213288fcd3fe
Chester Chiow <144107213+ChesterChiow@users.noreply.github.com>
2026-05-24 15:06:14 +0800

    Update: is-anagram - submission-2

--- Data Structures & Algorithms/is-anagram/submission-2.py
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -2,15 +2,14 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        countS, countT = {}, {}
 
-        count = [0] * 26 # [0,0,0,0,...]
+        countS = defaultdict(int)
+        countT = defaultdict(int)
+
         for i in range(len(s)):
-            count[ord(s[i])-ord('a')] += 1 # increase count if in s
-            count[ord(t[i])-ord('a')] -= 1 # decrease count if in t
-    
-        for val in count:
-            if (val != 0):
-                return False
-        return True
+            countS[s[i]] += 1
+            countT[t[i]] += 1
+        
+        if countS == countT:
+            return True
+        return False
